36f9f12a8674cc0a71d870e706b90d7ffa7b63e0
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-06-28 21:35:34 +0200

    Some colour for the soul.

--- golden.py
+++ golden.py
@@ -12,40 +12,39 @@ turtle.bgcolor('black')
 turtle.hideturtle()
 turtle.color('green')
 turtle.tracer(2, 0)
-time.sleep(5)
+#time.sleep(5)
 
 def triangle(t, side):
-    
     for _ in range(3):
-        t.forward(side)
-        t.left(120)
-    
-    
+       t.forward(side)
+       t.left(120)
+       t.dot(10)
 
-l1 = 1
-l2 = 1
+if __name__ == "__main__":
+
+    l1 = 1
+    l2 = 1
+    turtle.colormode(255)
 # turtle.penup()
 # turtle.goto(0,-100)
 # turtle.pendown()
-angle = 2
-side = 10900
-for i in range(0, 100):
-    
-    turtle.penup()
-    turtle.home()
-    side = side*0.95
-    side_bar = side*math.sin(math.pi/3)*math.sqrt(2)/2
-    x =   -math.cos(math.pi/6 + i*math.pi*angle/180)*side_bar
-    y =  -100 -math.sin(math.pi/6 + i*math.pi*angle/180)*side_bar
-    
-    turtle.goto(x,y)
-    #turtle.dot(10)
-    turtle.left(angle*i)
-    turtle.pendown()
-    
-    triangle(turtle, side)
-    
-    
-    
+    angle = 5
+    side = 10000
 
-turtle.mainloop()
+    for i in range(0, 255):
+       turtle.penup()
+       turtle.home()
+       side = side*0.95
+       side_bar = side*math.sin(math.pi/3)*math.sqrt(2)/2
+       x =   -math.cos(math.pi/6 + i*math.pi*angle/180)*side_bar
+       y =  -100 -math.sin(math.pi/6 + i*math.pi*angle/180)*side_bar
+
+       turtle.goto(x,y)
+    	#turtle.dot(10)
+       turtle.left(angle*i)
+       turtle.pendown()
+       turtle.pencolor(255-i, 30, i)
+       triangle(turtle, side)
+
+    time.sleep(1)
+
