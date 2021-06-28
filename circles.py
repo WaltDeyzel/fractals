acc0a6d931db1d2dcede201954ce21c3fa1e3643
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-06-28 22:41:28 +0200

    Quad circle finally working.

--- circles.py
+++ circles.py
@@ -10,7 +10,7 @@ window.screensize(1920,1080)
 rr = 1920/1080
 window.setup(width=1.0, height=1.0, startx=None, starty=None)
 
-tracer(1, 0)
+tracer(5, 0)
 n = 3 # number of points on circle
 rad = 2*math.pi/n;
 
@@ -25,13 +25,13 @@ def drawCircle(x,  y, radius):
   
   draw(x, y, radius)
   
-  if(radius > 20): 
+  if(radius > 10): 
     div = 2
-    print(x, y, radius)
-    drawCircle(x + radius, -y, radius/div);
-    drawCircle(x - radius, y, radius/div);
-    drawCircle(x, -(y + radius), radius/div);
-    drawCircle(x, -(y - radius), radius/div);
+    #print(x, y, radius)
+    drawCircle(x + radius, y+radius/2, radius/div);
+    drawCircle(x - radius, y+radius/2, radius/div);
+    drawCircle(x, (y + radius*1.5), radius/div);
+    drawCircle(x, (y - radius/2), radius/div);
 
 def triCircle(x, y, radius):
       
@@ -41,20 +41,11 @@ def triCircle(x, y, radius):
         triCircle(x, y+radius*1.5, radius*0.5)
         triCircle(x-radius, y-math.sin(2*math.pi/3), radius*0.5)
         triCircle(x+radius, y-math.sin(2*math.pi/3), radius*0.5)
-    
-  
-  
-   
-    
-  
-
-
-
 
 if __name__ == "__main__":
   #time.sleep(5)
-  r = 250
+  r = 500
   #triCircle(0, -350, r)
-  drawCircle(0, -150, r)
+  drawCircle(0, -r, r)
   print('DONE')
   done()
