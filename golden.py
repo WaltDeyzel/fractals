6a19ff091f97754d027c0c5152555df449cf88ed
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-07-01 13:19:28 +0200

    Draw any shape and make it spiral with new universal function.

--- golden.py
+++ golden.py
@@ -64,12 +64,68 @@ def goldenFibBlock(t, max_length, min_length, n, theta):
    for i in range(n):
       fibBlock(t, max_length, min_length)
       t.left(angle*i)
+
+def square(t, side_length):
    
+   for _ in range(4):
+      t.forward(side_length)
+      t.left(90)
+
+def goldenSquare(t, side_length, n):
+   angle = 3
+   side = side_length
+   for i in range(n):
+      t.color((random.randint(0,n), random.randint(0,n), random.randint(0,n)))
+      goHome(t)
+      side = side*0.95
+      side_bar = math.sqrt(side**2/2)
+      x =  -math.cos(math.pi/4 + i*math.pi*angle/180)*side_bar
+      y =  -math.sin(math.pi/4 + i*math.pi*angle/180)*side_bar
+      go(t, x, y)  
+      t.left(angle*i)
+      square(t, side)
+
+def shape(t, n, radius, rad ,offset):
+   
+   cords = []
+
+   for i in range(n):
+    
+      x = radius*math.cos(rad*i + offset)
+      y = radius*math.sin(rad*i + offset)
+      cords.append((x, y))
+   
+   t.penup()
+   for cor in cords:
+      t.setpos(cor)
+      t.pendown()      
+      t.dot(5)
+   t.setpos(cords[0])
+
+def universal(t, n, a):   
+   # N number of points 
+   # angle between each dot 
+   rad = 2*math.pi/n
+   offset = math.pi/2
+   radius = 700
+   
+   for i in range(100):
+      t.left(a*i)
+      shape(t, n, radius, rad, offset+a*i*math.pi/180)
+      radius = radius*0.98
+      goHome(t)
 
 if __name__ == "__main__":
    
-   goldenFibBlock(turtle, 800, 10, 170, 3)
-   goldenTriangle(turtle, 1000, 100)
-
-   time.sleep(3)
+   # goldenFibBlock(turtle, 800, 10, 170, 3)
+   # goldenTriangle(turtle, 1000, 100)
+   # goldenSquare(turtle, 1300, 100)
+   
+   for i in range(2, 20):
+      universal(turtle, i, 1) # turtle, shape, degree ofset
+      time.sleep(1)
+      window.reset()
+      turtle.hideturtle()
+      turtle.color('green')
+      turtle.tracer(3, 0)
 
